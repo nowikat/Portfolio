cf46c00d0159659c2c40f0b166904ab2364d2df7
nowikat <ketrin20@wp.pl>
2024-03-31 21:30:24 +0200

    codecademy

---
+++ codecademy/JavaScript/psielata.js
@@ -0,0 +1,8 @@
+const myAge = 31 // Tyle mam lat
+let earlyYears = 2 // Pierwsze lata życia psa
+earlyYears = earlyYears * 10.5
+let laterYears = myAge - 2 
+laterYears = laterYears * 4
+const myAgeInDogYears = laterYears + earlyYears
+myName= 'Kasia Nowicka' .toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`)
