9f0eaf129ed25459221b5ed3f61e392e5161d3d3
nowikat <ketrin20@wp.pl>
2024-04-15 19:08:31 +0200

    codecademy

---
+++ codecademy/JavaScript/eightBall.js
@@ -0,0 +1,26 @@
+let userName = '';
+userName ? console.log (`Hello, ${userName}!`) : console.log ('Hello!');
+const userQuestion = 'Will you become a millionaire in the future';
+console.log(`${userQuestion} ${userName}?`);
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = ''
+switch (randomNumber){
+  case eightBall = 1 : console.log('It is certain')
+  break;
+  case eightBall = 2 : console.log('It is decidedly so')
+  break;
+  case eightBall = 3 : console.log('Reply hazy try again') 
+  break;
+  case eightBall = 4 : console.log('Cannot predict now')
+  break ;
+  case eightBall = 5 : console.log ('Do not count on it')
+  break ;
+  case eightBall = 6 : console.log ('My sources say no')
+  break ;
+  case eightBall = 7 : console.log ('Outlook not so good') 
+  break ;
+  case eightBall = 8 : console.log ('Signs point to yes')
+  break ;
+  
+}
